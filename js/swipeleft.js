952cf1981144304bb5737c5c7c60da836d13bbc8
intrepidwebstudio <Intrepid-MacMini@Intrepid-Macs-Mac-mini.local>
2014-11-07 12:15:21 +0530

    1202

--- js/swipeleft.js
+++ js/swipeleft.js
@@ -102,7 +102,7 @@ $(function(){
 		    var change = e.originalEvent.pageX - x
 		    change = Math.min(Math.max(-250, change), 0 ) // restrict to -100px left, 0px right
             e.currentTarget.style.left = change + 'px'
-            if (change < -10) disable_scroll() // disable scroll once we hit 10px horizontal slide
+            if (change < -50) disable_scroll() // disable scroll once we hit 10px horizontal slide
 			}
         })
         .on('touchend', function(e) {
@@ -112,7 +112,7 @@ $(function(){
              left = parseInt(e.currentTarget.style.left)
 			
 			
-            var new_left = (left > -50 ? '0px' : '-250px') // snap back, or leave open, 50px threshold
+            var new_left = (left > -80 ? '0px' : '-250px') // snap back, or leave open, 50px threshold
             e.currentTarget.style.left = new_left
             enable_scroll()
 			
