5887c4a0e1683c6b05a80471ab44235422a451e0
intrepidwebstudio <Intrepid-MacMini@Intrepid-Macs-Mac-mini.local>
2014-11-10 17:38:32 +0530

    12544

--- js/swipeleft.js
+++ js/swipeleft.js
@@ -110,7 +110,7 @@ $(function(){
 												
 												}else{
 													
-												e.currentTarget.style.left = 0 + 'px';
+											//	e.currentTarget.style.left = 0 + 'px';
 												enable_scroll();	
 													
 													}
