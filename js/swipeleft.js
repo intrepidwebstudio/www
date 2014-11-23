a1255638c0abca6b00e64784dd9ea0b3571f3e28
intrepidwebstudio <Intrepid-MacMini@Intrepid-Macs-Mac-mini.local>
2014-11-24 04:13:18 +0530

    5

--- js/swipeleft.js
+++ js/swipeleft.js
@@ -161,7 +161,17 @@ function hex(x) {
 			else if( media_link == "email"  )
 			{
 				
-			window.plugins.socialsharing.shareViaEmail(title_aaa , null /* img */,title_linkkk );
+				window.plugins.socialsharing.shareViaEmail(
+  title_aaa + title_linkkk, // can contain HTML tags, but support on Android is rather limited:  http://stackoverflow.com/questions/15136480/how-to-send-html-content-with-image-through-android-default-email-client
+  'Check out this article',
+null, // TO: must be null or an array
+  null, // CC: must be null or an array
+  null, // BCC: must be null or an array
+  null // FILES: can be null, a string, or an array
+  
+);
+				
+		//	window.plugins.socialsharing.shareViaEmail(title_aaa , null /* img */,title_linkkk );
 				}
 				
 			else if( media_link == "tumblr") 
